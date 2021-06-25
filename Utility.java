5508f20e56cd98efe6c973c83a0b049c572dc94d
deepakmench18 <80206715+deepakmench18@users.noreply.github.com>
2021-06-25 23:27:12 +0530

    Stop Watch Program

--- Utility.java
+++ Utility.java
@@ -1,33 +1,5 @@
 package com.bridgelab.utility;
 
-import java.util.Scanner;
-
 public class Utility {
-	Scanner scanner;
-	public Utility()
-	{
-		scanner = new Scanner(System.in);
-	}
-	public double getDouble()
-	{
-		 return scanner.nextDouble();
-	}
-	public boolean getBoolean()
-	{
-		return scanner.nextBoolean();
-	}
-	public int getValue()
-	{
-		return scanner.nextInt();
-	}
-	public <T> void print2DArray(T[][] inputArray, int rows, int columns) {
-		for (int i =0;i<rows;i++)
-		{
-			for (int j =0;j<columns;j++)
-			{ 
-				System.out.print(inputArray[i][j]+" ");
-			}
-			System.out.println(" ");
-		}
-	}
+
 }
